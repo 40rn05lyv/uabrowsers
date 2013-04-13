c2b7e9b7a2c00c99f80d2b221bf824be728d6901
Andrii Pitukh <andriy.pitukh@gmail.com>
2013-04-13 18:37:39 +0300

    Adding comments

--- UkrainizeBrowsers/src/browsers/windows/StartUkrainize.java
+++ UkrainizeBrowsers/src/browsers/windows/StartUkrainize.java
@@ -13,26 +13,35 @@ public class StartUkrainize {
 	static String users = "Users";
 	static String user = System.getProperty("user.name");
 
-	/**
-	 * @param args
-	 * @throws IOException
-	 * @throws InvocationTargetException
-	 * @throws IllegalAccessException
-	 * @throws IllegalArgumentException
-	 */
-	public static void main(String[] langs) throws IOException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
-		ukrainizeOpera(langs);
-		ukrainizeFirefox(langs);
-		ukrainizeChrome(langs);
-		ukrainizeIE(langs);
+	private static String addQParameter(String[] langs) {
+		String[] langsWithQ = new String[langs.length];
+		double q = 1;
+		for (int i = 0; i < langs.length; i++) {
+			langsWithQ[i] = langs[i];
+			if (q != 1) {
+				langsWithQ[i] += ";q=" + q;
+			}
+			q /= 2;
+		}
+		return join(",", langsWithQ);
 	}
 
-	private static void ukrainizeIE(String[] langs) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
-		String setting = addQParameter(langs); // With q parameter
-		WinRegistry.writeStringValue(WinRegistry.HKEY_CURRENT_USER, "SOFTWARE\\Microsoft\\Internet Explorer\\International", "AcceptLanguage",
-				setting);
-		System.out.println("IE was ukrainized");
-		System.out.println();
+	private static File createFile(File file, String... folders) {
+		StringBuilder folderPath = new StringBuilder(file.getAbsolutePath() + "\\");
+		for (String folder : folders) {
+			folderPath.append(folder);
+			folderPath.append("\\");
+		}
+		return new File(folderPath.toString());
+	}
+
+	private static File createFile(String drive, String... folders) {
+		StringBuilder folderPath = new StringBuilder(drive + "\\");
+		for (String folder : folders) {
+			folderPath.append(folder);
+			folderPath.append("\\");
+		}
+		return new File(folderPath.toString());
 	}
 
 	private static String join(String separator, String... args) {
@@ -45,47 +54,43 @@ public class StartUkrainize {
 		return result.toString();
 	}
 
-	/**
-	 * @param args
-	 * @throws IOException
+	/***
+	 * Entry point
 	 */
-	public static void ukrainizeOpera(String[] langs) throws IOException {
-		String setting = addQParameter(langs); // With q parameter
+	public static void main(String[] langs) throws IOException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+		ukrainizeOpera(langs);
+		ukrainizeFirefox(langs);
+		ukrainizeChrome(langs);
+		ukrainizeIE(langs);
+	}
 
-		File first = createFile(drive, users, user, "AppData", "Roaming", "Opera", "Opera x64", "operaprefs.ini");
-		File second = createFile(drive, users, user, "AppData", "Roaming", "Opera", "Opera", "operaprefs.ini");
-
-		if (first.exists()) {
-			String text = FileUtils.readFileToString(first);
-			text = text.replaceAll("HTTP Accept Language.*", "HTTP Accept Language=" + setting);
-			FileUtils.writeStringToFile(first, text);
-			System.out.println("Opera was ukrainized");
-			System.out.println("Path: " + first.getAbsolutePath());
-			System.out.println();
-		}
-		if (second.exists()) {
-			String text = FileUtils.readFileToString(second);
-			text = text.replaceAll("HTTP Accept Language.*", "HTTP Accept Language=" + setting);
-			FileUtils.writeStringToFile(second, text);
-			System.out.println("Opera was ukrainized");
-			System.out.println("Path: " + second.getAbsolutePath());
+	/***
+	 * Chrome settings are stored in
+	 * C:/Users/[Current User]/AppData/Local/Google/Chrome/User Data/Default/Preferences
+	 * Parameter: accept_languages
+	 * <b>NOTE: Chrome need to be closed when executing this function
+	 * NOTE: Works only if Chrome is not tight to some account</b>
+	 */
+	public static void ukrainizeChrome(String[] langs) throws IOException {
+		String setting = join(",", langs); // Without q parameter
+		File folder = createFile(drive, users, user, "AppData", "Local", "Google", "Chrome", "User Data", "Default", "Preferences");
+		if (folder.exists()) {
+			String text = FileUtils.readFileToString(folder);
+			text = text.replaceAll("accept_languages.*", "accept_languages\": \"" + setting + "\"");
+			FileUtils.writeStringToFile(folder, text);
+			System.out.println("Chrome was ukrainized");
+			System.out.println("Path: " + folder.getAbsolutePath());
 			System.out.println();
 		}
 	}
 
-	private static String addQParameter(String[] langs) {
-		String[] langsWithQ = new String[langs.length];
-		double q = 1;
-		for (int i=0; i<langs.length; i++) {
-			langsWithQ[i] = langs[i];
-			if (q!=1) {
-				langsWithQ[i] += ";q=" + q;
-			}
-			q /= 2;
-		}
-		return join(",", langsWithQ);
-	}
-
+	/***
+	 * Firefox settings are stored in
+	 * C:/Users/[Current User]/AppData/Roaming/Mozilla/Firefox/Profiles/[some-folder-with-autogen-name]/prefs.js
+	 * Parameter: intl.accept_languages
+	 * This file is just a javascript file
+	 * <b>NOTE: Firefox needs to be closed when executing this function</b>
+	 */
 	public static void ukrainizeFirefox(String[] langs) throws IOException {
 		final String firefoxSettings = "prefs.js";
 		String setting = join(",", langs); // Without q parameter
@@ -114,37 +119,50 @@ public class StartUkrainize {
 		}
 	}
 
-	/*
-	 * Works only if Chrome is not tight to some account
+	/***
+	 * IE settings are saved in Windows Registry and can be accessed by:
+	 * HKEY_CURRENT_USER\SOFTWARE\Microsoft\Internet Explorer\International
+	 * Parameter: AcceptLanguage
+	 * If this parameter doesn't exist then some default value will be used (TODO: where is default value stored??)
+	 * I have to create or overwrite AcceptLanguage parameter.
+	 * <b>NOTE: browser does NOT have to be turned off when modifying registry!</b>
 	 */
-	public static void ukrainizeChrome(String[] langs) throws IOException {
-		String setting = join(",", langs); // Without q parameter
-		File folder = createFile(drive, users, user, "AppData", "Local", "Google", "Chrome", "User Data", "Default", "Preferences");
-		if (folder.exists()) {
-			String text = FileUtils.readFileToString(folder);
-			text = text.replaceAll("accept_languages.*", "accept_languages\": \"" + setting + "\"");
-			FileUtils.writeStringToFile(folder, text);
-			System.out.println("Chrome was ukrainized");
-			System.out.println("Path: " + folder.getAbsolutePath());
+	private static void ukrainizeIE(String[] langs) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+		String setting = addQParameter(langs); // With q parameter
+		WinRegistry.writeStringValue(WinRegistry.HKEY_CURRENT_USER, "SOFTWARE\\Microsoft\\Internet Explorer\\International", "AcceptLanguage",
+				setting);
+		System.out.println("IE was ukrainized");
+		System.out.println();
+	}
+
+	/***
+	 * Opera settings are saved in:
+	 * 1. C:/Users/[CurrentUser]/AppData/Roaming/Opera/Opera x64/operaprefs.ini
+	 * 2. C:/Users/[CurrentUser]/AppData/Roaming/Opera/Opera/operaprefs.ini
+	 * Parameter: HTTP Accept Language
+	 * <b>NOTE: Opera needs to be closed when executing this function</b>
+	 */
+	public static void ukrainizeOpera(String[] langs) throws IOException {
+		String setting = addQParameter(langs); // With q parameter
+
+		File first = createFile(drive, users, user, "AppData", "Roaming", "Opera", "Opera x64", "operaprefs.ini");
+		File second = createFile(drive, users, user, "AppData", "Roaming", "Opera", "Opera", "operaprefs.ini");
+
+		if (first.exists()) {
+			String text = FileUtils.readFileToString(first);
+			text = text.replaceAll("HTTP Accept Language.*", "HTTP Accept Language=" + setting);
+			FileUtils.writeStringToFile(first, text);
+			System.out.println("Opera was ukrainized");
+			System.out.println("Path: " + first.getAbsolutePath());
 			System.out.println();
 		}
-	}
-
-	private static File createFile(File file, String... folders) {
-		StringBuilder folderPath = new StringBuilder(file.getAbsolutePath() + "\\");
-		for (String folder : folders) {
-			folderPath.append(folder);
-			folderPath.append("\\");
+		if (second.exists()) {
+			String text = FileUtils.readFileToString(second);
+			text = text.replaceAll("HTTP Accept Language.*", "HTTP Accept Language=" + setting);
+			FileUtils.writeStringToFile(second, text);
+			System.out.println("Opera was ukrainized");
+			System.out.println("Path: " + second.getAbsolutePath());
+			System.out.println();
 		}
-		return new File(folderPath.toString());
-	}
-
-	private static File createFile(String drive, String... folders) {
-		StringBuilder folderPath = new StringBuilder(drive + "\\");
-		for (String folder : folders) {
-			folderPath.append(folder);
-			folderPath.append("\\");
-		}
-		return new File(folderPath.toString());
 	}
 }
